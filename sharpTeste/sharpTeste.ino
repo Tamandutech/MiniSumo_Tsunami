39c100b99223af7df2df40c8f4ffbdef45b4cbb3
luccaki <49926738+luccaki@users.noreply.github.com>
2020-02-14 20:16:53 -0300

    Update sharpTeste.ino

--- sharpTeste/sharpTeste.ino
+++ sharpTeste/sharpTeste.ino
@@ -1,26 +1,64 @@
+#include <analogWrite.h>
+/*#include <MPU6050_tockn.h>
+#include <Wire.h>
+MPU6050 mpu(Wire);*/
 
-#include <SharpDistSensor.h>
-
-// Analog pin to which the sensor is connected
-const byte sensorPin = 12;
-
-// Window size of the median filter (odd number, 1 = no filtering)
-const byte medianFilterWindowSize = 5;
-
-// Create an object instance of the SharpDistSensor class
-SharpDistSensor sensor(sensorPin, medianFilterWindowSize);
-
-
-void setup() {
-  Serial.begin(9600);
-  pinMode(sensorPin, INPUT);
-}
+  #define sharpF 12
+  #define sharpE 13
+  #define sharpD 13
   
+  
+  #define pwmB 23
+  #define b1 5
+  #define b2 1
+  #define stby 3
+  #define a1 26
+  #define a2 19
+  #define pwmA 18
+  //sensor de linha
+  #define linha1 2
+  #define linha2 4
+  //acelerometro
+  float yInicial = 0;
+  //potencia
+  int a = 100;
+  int b = 100;
+  bool direc = true;
+  
+void setup() {
+  pinMode(pwmB, OUTPUT);
+  pinMode(b1, OUTPUT);
+  pinMode(b2, OUTPUT);
+  pinMode(stby, OUTPUT);
+  pinMode(a1, OUTPUT);
+  pinMode(a2, OUTPUT);
+  pinMode(pwmA, OUTPUT);
+  pinMode(linha1, INPUT);
+  pinMode(linha2, INPUT);
+  pinMode(sharpF, INPUT);
+  pinMode(sharpE, INPUT);
+  pinMode(sharpD, INPUT);
+  digitalWrite(stby,1);
+  Serial.begin(9600);
+}
+ float media = 0;
+ float cont = 0;
+ int i = 0;
+
+ int media10(int i){
+    int media=0;
+    for(int a=0;a<1000;a++){
+        media+=analogRead(i);
+      }
+    return media/1000;
+  
+  }
+ 
 void loop() {
-  // Get distance from sensor
-  unsigned int distance = sensor.getDist();
-  // Print distance to Serial
-  Serial.println(distance);
-  // Wait some time
-  delay(50);
+  Serial.print(media10(13));
+  Serial.print(" ");
+  Serial.print(media10(12));
+  Serial.print(" ");
+  Serial.println(media10(14));
+  delay(500);
 }
